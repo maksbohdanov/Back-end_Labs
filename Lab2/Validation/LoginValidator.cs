0036bec57d9778c85891542cddbddbbb22d210a6
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-12-14 14:24:24 +0200

    Update validation rules due to new models

---
+++ Lab2/Validation/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WebApi.Models;
+
+namespace CostAccounting.Validation
+{
+    public class LoginValidator : AbstractValidator<LoginModel>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }        
+    }
+}
---
+++ Lab2/Validation/RegistrationValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using WebApi.Models;
+
+namespace CostAccounting.Validation
+{
+    public class RegistrationValidator : AbstractValidator<RegistrationModel>
+    {
+        public RegistrationValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+               .NotEmpty()
+               .MinimumLength(6);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(30);
+        }        
+    }
+}
